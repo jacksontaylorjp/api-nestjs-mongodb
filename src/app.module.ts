0f55cd276af902eb36b795b6a64228aba6cdf5b1
jackson <jacksontaylorjp@gmail.com>
2025-07-05 20:02:48 -0300

    feat: module e schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,15 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ItemModule } from './item/item.module';
 
 @Module({
   imports: [
-    // Carrega o .env automaticamente
     ConfigModule.forRoot({
-      isGlobal: true, // Faz o ConfigService funcionar em toda a aplicação
+      isGlobal: true, 
     }),
 
-    // Conecta ao MongoDB usando a variável do .env
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
@@ -19,6 +18,8 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
         uri: configService.get<string>('MONGO_URI'),
       }),
     }),
+
+    ItemModule,
   ],
   controllers: [AppController],
   providers: [AppService],
