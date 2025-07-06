2143211ec5d3882e158a3c27a3319ee0ec45739a
jackson <jacksontaylorjp@gmail.com>
2025-07-05 21:20:09 -0300

    feat: crud

--- src/item/item.module.ts
+++ src/item/item.module.ts
@@ -1,12 +1,16 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Item, ItemSchema } from './item.schema';
+import { ItemService } from './item.service';
+import { ItemController } from './item.controller';
 
 @Module({
     imports: [
         MongooseModule.forFeature([{
             name: Item.name, schema: ItemSchema
         }])
-    ]
+    ],
+    providers: [ItemService],
+    controllers: [ItemController]
 })
 export class ItemModule { }
